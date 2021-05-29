832f955cb48b559815ab0f8c887b4c93d750e92d
PasinduS96 <pasindubahagya@gmail.com>
2021-05-29 20:59:20 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require("dotenv").config();
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+app.use(cors())
+
+app.listen(process.env.PORT, () =>{
+
+    console.log(`server started at ${process.env.PORT}`);
+
+    //Connect to the database
+    mongoose.Promise = global.Promise;
+    mongoose
+    .connect(process.env.MONGO_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, (err, db) =>{
+        console.log("MongoDB Connected Successfully!!")
+        
+    })
+})
